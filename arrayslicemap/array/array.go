fdffa5ea3a58d10541d6b27b0e7e8ed641b5b1c4
Thiago Leite <thiago.leite@agileengine.com>
2022-07-20 12:49:17 -0300

    Trabalhando com Array

---
+++ arrayslicemap/array/array.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func calcMedia(notas [3]float64) {
+	total := 0.0
+	for i := 0; i < len(notas); i++ {
+		total += notas[i]
+	}
+
+	media := total / float64(len(notas))
+	fmt.Printf("Média %.2f\n", media)
+}
+
+func main() {
+	var notas [3]float64
+	fmt.Println(notas)
+	calcMedia(notas)
+
+	notas[0], notas[1], notas[2] = 7.8, 4.3, 9.1
+	fmt.Println(notas)
+	calcMedia(notas)
+
+}
