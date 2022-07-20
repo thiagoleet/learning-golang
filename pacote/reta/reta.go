88700ef4acb50cedf8d6e01df230b781353f7fd1
Thiago Leite <thiago.leite@agileengine.com>
2022-07-20 18:34:19 -0300

    Criando um pacote

---
+++ pacote/reta/reta.go
@@ -0,0 +1,24 @@
+package main
+
+import "math"
+
+// Incializando com letra maiúscula é PÚBLICO (visível dentro e fora do pacote)
+// Inicializando com letra minúsculam é PRIVADO (visível apenas dentro do pacote)
+
+// Representa uma coordenada no plano cartesiano
+type Ponto struct {
+	x float64
+	y float64
+}
+
+func catetos(a, b Ponto) (cx, cy float64) {
+	cx = math.Abs(b.x - a.x)
+	cy = math.Abs(b.y - a.y)
+	return
+}
+
+// Distância é responsável por calcular a distância linear entre dois pontos
+func Distancia(a, b Ponto) float64 {
+	cx, cy := catetos(a, b)
+	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))
+}
