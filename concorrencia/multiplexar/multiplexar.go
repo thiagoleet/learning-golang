bd177b5c610811b00c0f4eb3b1d6a357055a6cae
Thiago Leite <thiago.leite@agileengine.com>
2022-07-20 22:13:21 -0300

    Concorrência: multiplex

---
+++ concorrencia/multiplexar/multiplexar.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/thiagoleet/gohtml"
+)
+
+func encaminhar(origem <-chan string, destino chan string) {
+	for {
+		destino <- <-origem
+	}
+}
+
+// multiplexar - misturar (mensagens) num canal
+func juntar(entrada1, entrada2 <-chan string) <-chan string {
+	c := make(chan string)
+	go encaminhar(entrada1, c)
+	go encaminhar(entrada2, c)
+	return c
+}
+
+func main() {
+	c := juntar(
+		gohtml.Titulo("https://www.cod3r.com.br", "https://www.google.com"),
+		gohtml.Titulo("https://www.amazon.com", "https://www.youtube.com"),
+	)
+	fmt.Println(<-c, "|", <-c)
+	fmt.Println(<-c, "|", <-c)
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.18
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/thiagoleet/goarea v0.0.0-20220720215018-947edbd93380 // indirect
+	github.com/thiagoleet/gohtml v0.0.0-20220721011154-aeaf0675d280 // indirect
 )
